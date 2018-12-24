fdf85e28b5c31c9e729d31cac4a3c0751c9fa159
Dimitri mcDaniel <ibmiti@Dimitris-MacBook-Air.local>
2018-12-23 21:01:23 -0500

    adding onClick={()} to renderSquares(i) { ..  what this will do is handle any click and pass a value to its state and save the state onto the board

--- src/index.js
+++ src/index.js
@@ -33,8 +33,13 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={i} />;
-  }
+    return (
+       <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+     />
+  );
+}
 
   render() {
     const status = 'Next player: X';
